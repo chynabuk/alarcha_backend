b96a8f6d654b53252752c8fa35be2eb45c0c1cfd
kubanychbek <kushtarbekovkuba@gmail.com>
2022-03-23 19:26:05 +0600

    .

--- src/main/java/com/project/alarcha/entities/User.java
+++ src/main/java/com/project/alarcha/entities/User.java
@@ -17,6 +17,7 @@ public class User extends BaseEntity{
     @Column(name = "first_name", nullable = false)
     private String firstName;
 
+
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
