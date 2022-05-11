78f0fab904dda39e7a9a9b9be9d979e1bf0ecbb1
kubanychbek <kushtarbekovkuba@gmail.com>
2022-05-11 22:44:52 +0600

    roomType img 2

--- src/main/java/com/project/alarcha/service/impl/RoomTypeServiceImpl.java
+++ src/main/java/com/project/alarcha/service/impl/RoomTypeServiceImpl.java
@@ -240,8 +240,11 @@ public class RoomTypeServiceImpl implements RoomTypeService {
         if (roomType.getRooms() != null){
             roomTypeModel.setRoomModels(roomService.getByRoomType(roomType));
         }
-        if (roomType.getRoomTypeImages() != null){
-            roomTypeModel.setRoomTypeImageModels(roomTypeImageService.convertToModels(roomType.getRoomTypeImages()));
+        if (!roomType.getRoomTypeImages().isEmpty()){
+            if(roomType.getRoomTypeImages().size() > 2){
+                roomTypeModel.setRoomTypeImageModels(roomTypeImageService
+                        .convertToModels(roomType.getRoomTypeImages().subList(1, roomType.getRoomTypeImages().size())));
+            }
         }
 
         return roomTypeModel;
