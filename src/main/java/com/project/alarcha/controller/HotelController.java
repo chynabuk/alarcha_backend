464f943c31e77c06070f0674de4d4ddaddd9bb71
kubanychbek <kushtarbekovkuba@gmail.com>
2022-05-30 23:14:20 +0600

    ..

--- src/main/java/com/project/alarcha/controller/HotelController.java
+++ src/main/java/com/project/alarcha/controller/HotelController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin("http://localhost:8080")
 @RequestMapping("/hotel")
 public class HotelController {
     @Autowired
