9c22ef0f068a7d7683a1067f3538ca1eab6f4d18
kubanychbek <kushtarbekovkuba@gmail.com>
2022-05-31 21:44:06 +0600

    ..

--- src/main/java/com/project/alarcha/controller/BlogController.java
+++ src/main/java/com/project/alarcha/controller/BlogController.java
@@ -5,10 +5,7 @@ import com.project.alarcha.models.BlogModel.BlogModel;
 import com.project.alarcha.service.BlogService;
 import com.project.alarcha.util.ResponseMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
--- src/main/java/com/project/alarcha/controller/MainController.java
+++ src/main/java/com/project/alarcha/controller/MainController.java
@@ -6,10 +6,7 @@ import com.project.alarcha.service.CounterService;
 import com.project.alarcha.service.Population;
 import com.project.alarcha.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
